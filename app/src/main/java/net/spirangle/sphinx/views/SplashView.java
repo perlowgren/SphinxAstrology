555c0872c3964a26fa630aea807b32120cbffa51
Per Löwgren <per.lowgren@gmail.com>
2021-08-04 18:50:12 +0200

    Update drawing of Spirangle Studio presents Sphinx Astrology.

--- app/src/main/java/net/spirangle/sphinx/views/SplashView.java
+++ app/src/main/java/net/spirangle/sphinx/views/SplashView.java
@@ -16,7 +16,8 @@ public class SplashView extends View {
     private static final String TAG = "SplashView";
 
     private Paint paint = new Paint();
-    private String spiranglePresents = null;
+    private String spirangleStudio = null;
+    private String presents = null;
     private String appName = null;
     private String labelFormat = null;
     private String label = null;
@@ -43,7 +44,8 @@ public class SplashView extends View {
 	}*/
 
     private void init(Context context) {
-        spiranglePresents = context.getString(R.string.spirangle_presents);
+        spirangleStudio = context.getString(R.string.spirangle_studio);
+        presents = context.getString(R.string.presents);
         appName = context.getString(R.string.title_app);
         labelFormat = context.getString(R.string.installing);
     }
@@ -52,32 +54,43 @@ public class SplashView extends View {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Context context = getContext();
-        float x, y, w = (float)getWidth(), h = (float)getHeight();
+        float x,y;
+        float w = (float)getWidth();
+        float h = (float)getHeight();
+        float s = Math.min(w,h);
+        float textSize1 = s*0.04f;
+        float textSize2 = s*0.07f;
+        float textSize3 = s*0.03f;
         String str;
-        Log.d(APP,TAG+".onDraw()");
-        paint.setStyle(Paint.Style.FILL);
+        Log.d(APP,TAG+".onDraw(w="+w+",h="+h+")");
         paint.setAntiAlias(true);
+        paint.setColor(0xffff00ff);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(1.0f,1.0f,w-2.0f,h-2.0f,paint);
         paint.setColor(0xffffcc99);
-        paint.setTextSize(16);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(textSize1);
         x = w*0.5f;
-        y = h*0.5f+180.0f;
-        str = spiranglePresents;
+        y = h*0.75f-s*0.04f-textSize1;
+        str = spirangleStudio;
         canvas.drawText(str,x-paint.measureText(str)*0.5f,y,paint);
-        paint.setTextSize(30);
-        y += 40.0f;
+        y = h*0.75f;
+        str = presents;
+        canvas.drawText(str,x-paint.measureText(str)*0.5f,y,paint);
+        paint.setTextSize(textSize2);
+        y = h*0.75f+s*0.04f+textSize2;
         str = appName;
         canvas.drawText(str,x-paint.measureText(str)*0.5f,y,paint);
         if(progress>0.0f && progress<1.0f && label!=null) {
-            paint.setTextSize(16);
-            canvas.drawText(label,5.0f,h-10.0f,paint);
-            canvas.drawRect(0.0f,h-5.0f,w*progress,h,paint);
+            paint.setTextSize(textSize3);
+            canvas.drawText(label,s*0.01f,h-s*0.025f,paint);
+            canvas.drawRect(0.0f,h-s*0.015f,w*progress,h,paint);
         }
     }
 
-    public void setProgress(float p,String l) {
-        if(l!=null)
-            label = String.format(labelFormat,l);
-        progress = p;
+    public void setProgress(float progress,String label) {
+        if(label!=null) this.label = String.format(labelFormat,label);
+        this.progress = progress;
         invalidate();
     }
 }
